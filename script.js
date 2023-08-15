4d73f1beb165072092ddd9c960716cf9a574c6ac
Ivanna Yarynovska <ivanna.yarynovsk@gmail.com>
2023-08-14 23:28:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+
+// 3.5 Implement the createTaskElement(taskObj) Function
+function createTaskElement(taskObj) {
+    const taskItem = document.createElement("li");
+    taskItem.classList.add("taskItem");
+  
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("checkbox");
+    checkbox.checked = taskObj.completed;
+    checkbox.addEventListener("change", function () {
+        taskObj.completed = checkbox.checked;
+        taskTextElement.classList.toggle("completed", taskObj.completed);
+        updateTasksInLocalStorage();
+    });
+  
+    const taskTextElement = document.createElement("span");
+    taskTextElement.classList.add("taskText");
+    taskTextElement.textContent = taskObj.text;
+    taskTextElement.classList.toggle("completed", taskObj.completed);
+  
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("removeButton");
+    removeButton.textContent = "X";
+    removeButton.addEventListener("click", function () {
+        deleteTask(taskObj);
+        renderTasks();
+    });
+  
+    taskItem.appendChild(checkbox);
+    taskItem.appendChild(taskTextElement);
+    taskItem.appendChild(removeButton);
+  
+    return taskItem;
+}
